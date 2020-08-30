bd1c5927d527be7fd36fa11a6bf1061672a185e8
Rafael Hirooka <rafaelhirooka16@gmail.com>
2020-08-30 16:29:50 -0300

    appointment and config typeorm

---
+++ src/routes/appointments.routes.ts
@@ -0,0 +1,36 @@
+import { Router } from 'express';
+import { parseISO } from 'date-fns';
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
+import CreateAppointmentService from '../services/CreateAppointmentService';
+
+const appointmentsRepository = new AppointmentsRepository();
+
+const appoinmentsRouter = Router();
+
+appoinmentsRouter.get('/', (req, res) => {
+  const appointments = appointmentsRepository.all();
+  return res.json(appointments);
+});
+
+appoinmentsRouter.post('/', (req, res) => {
+  try {
+    const { provider, date } = req.body;
+
+    const parsedDate = parseISO(date);
+
+    const createAppointment = new CreateAppointmentService(
+      appointmentsRepository,
+    );
+
+    const appointment = createAppointment.execute({
+      date: parsedDate,
+      provider,
+    });
+
+    return res.json(appointment);
+  } catch (err) {
+    return res.status(400).json({ error: err });
+  }
+});
+
+export default appoinmentsRouter;
