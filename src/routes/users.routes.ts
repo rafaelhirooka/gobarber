6c96e85a87673c7b9c2dc7f2020188974ff2dc1c
Rafael Hirooka <rafaelhirooka16@gmail.com>
2020-09-01 22:38:45 -0300

    delete user password on response

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -41,6 +41,8 @@ usersRouter.patch(
         avatarFilename: req.file.filename,
       });
 
+      delete user.password;
+
       return res.json(user);
     } catch (err) {
       return res.status(400).json({ error: err.message });
