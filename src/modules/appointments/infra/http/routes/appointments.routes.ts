86a1b1c0943663f6eb6ccc6cbc23d00b66905ee6
Rafael Hirooka <rhirooka@integrationconsulting.com>
2020-11-12 20:53:19 -0300

    eslint and appointments

--- src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -1,26 +1,27 @@
 import { Router } from 'express';
 import { parseISO } from 'date-fns';
-import { getCustomRepository } from 'typeorm';
 import AppointmentsRepository from '@modules/appointments/infra/typeorm/repositories/AppointmentsRepository';
 import CreateAppointmentService from '@modules/appointments/services/CreateAppointmentService';
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 
 const appoinmentsRouter = Router();
+const appointmentsRepository = new AppointmentsRepository();
 
 appoinmentsRouter.use(ensureAuthenticated);
 
-appoinmentsRouter.get('/', async (req, res) => {
-  const appointmentsRepository = getCustomRepository(AppointmentsRepository);
-  const appointments = await appointmentsRepository.find();
-  return res.json(appointments);
-});
+// appoinmentsRouter.get('/', async (req, res) => {
+//   const appointments = await appointmentsRepository.find();
+//   return res.json(appointments);
+// });
 
 appoinmentsRouter.post('/', async (req, res) => {
   const { providerId, date } = req.body;
 
   const parsedDate = parseISO(date);
 
-  const createAppointment = new CreateAppointmentService();
+  const createAppointment = new CreateAppointmentService(
+    appointmentsRepository,
+  );
 
   const appointment = await createAppointment.execute({
     date: parsedDate,
